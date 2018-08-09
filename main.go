ad554c9ede0878d983cb84aaa3233828a52f0533
Ismail Mustafa <ismailmustafa@rocketmail.com>
2018-08-09 19:22:39 -0400

    Show help when no args passed in

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/fatih/color"
@@ -13,36 +12,9 @@ func main() {
 	err := initiateDatabaseIfNeeded()
 	if err != nil {
 		color.HiRed("Error initiating database file '" + databaseFilename + "' (" + err.Error() + ")")
-		return
-	}
-	if len(os.Args) == 1 {
-		fmt.Println(color.HiWhiteString("Launching user interface..."))
+	} else if len(os.Args) == 1 {
+		displayUsage()
 	} else {
 		cli(os.Args[1:])
 	}
 }
-
-// func displayTime(seconds float64) string {
-// 	formater := "%.1f %s"
-// 	minute := float64(60)
-// 	hour := minute * float64(60)
-// 	day := hour * float64(24)
-// 	month := day * float64(31)
-// 	year := month * float64(12)
-// 	century := year * float64(100)
-// 	if seconds < minute {
-// 		return "a few seconds"
-// 	} else if seconds < hour {
-// 		return fmt.Sprintf(formater, (1 + math.Ceil(seconds/minute)), "minutes")
-// 	} else if seconds < day {
-// 		return fmt.Sprintf(formater, (1 + math.Ceil(seconds/hour)), "hours")
-// 	} else if seconds < month {
-// 		return fmt.Sprintf(formater, (1 + math.Ceil(seconds/day)), "days")
-// 	} else if seconds < year {
-// 		return fmt.Sprintf(formater, (1 + math.Ceil(seconds/month)), "months")
-// 	} else if seconds < century {
-// 		return fmt.Sprintf(formater, (1 + math.Ceil(seconds/century)), "years")
-// 	} else {
-// 		return "centuries"
-// 	}
-// }
