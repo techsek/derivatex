6cc449f4e8d4a6327d24f40e171d14f68928b494
Quentin McGaw <quentin.mcgaw@gmail.com>
2018-08-14 14:22:47 +0200

    Changed argon creation params and file names

--- constants/constants.go
+++ constants/constants.go
@@ -1,15 +1,15 @@
 package constants
 
 // Only used to reconstruct master digest from master password and birthdate
-const ArgonMemoryMB uint32 = 2048 // the more the better, depending on your machine
+const ArgonMemoryMB uint32 = 512 // the more the better, depending on your machine
 const ArgonDigestSize uint32 = 64
-const ArgonTimeCost uint32 = 130
-const ArgonParallelism uint8 = 1
-const ArgonTestRounds uint32 = 1
+const ArgonTimeCost uint32 = 5000
+const ArgonParallelism uint8 = 4
+const ArgonTestRounds uint32 = 100
 
-const MasterDigestFilename = "secret_digest.txt"
+const MasterDigestFilename = "seed.txt"
 const DefaultPasswordLength = 20
-const DatabaseFilename = "database"
+const DatabaseFilename = "database.sqlite"
 const DefaultTableToDump = "identifications"
 
 const Version = 2
