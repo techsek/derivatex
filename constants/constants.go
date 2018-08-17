fc4f7911974026a91bbacf52278b1db80f856646
Quentin McGaw <quentin.mcgaw@gmail.com>
2018-08-17 15:41:33 +0200

    Added Argon hashing for passphrases

--- constants/constants.go
+++ constants/constants.go
@@ -7,6 +7,12 @@ const ArgonTimeCost uint32 = 5000
 const ArgonParallelism uint8 = 4
 const ArgonTestRounds uint32 = 100
 
+// Argon2ID settings for the optional passphrase to encrypt the seed
+const PassphraseArgonMemoryMB uint32 = 100
+const PassphraseArgonDigestSize uint32 = 32
+const PassphraseArgonTimeCost uint32 = 10
+const PassphraseArgonParallelism uint8 = 4
+
 const SeedFilename = "seed.txt"
 const DefaultPasswordLength = 20
 const DatabaseFilename = "database.sqlite"
--- internal/hashing.go
+++ internal/hashing.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"github.com/techsek/derivatex/constants"
+	"golang.org/x/crypto/argon2"
 	"golang.org/x/crypto/sha3"
 )
 
@@ -13,5 +15,11 @@ func HashAndDestroy(data *[]byte) (digest *[32]byte) {
 func HashSHA3_256(data *[]byte) (digest *[32]byte) {
 	digest = new([32]byte)
 	*digest = sha3.Sum256(*data)
+	return digest
+}
+
+func HashArgon2ID_256(source *[32]byte, salt *[32]byte) (digest *[]byte) {
+	digest = new([]byte)
+	*digest = argon2.IDKey((*source)[:], (*salt)[:], constants.PassphraseArgonTimeCost, constants.PassphraseArgonMemoryMB*1024, constants.PassphraseArgonParallelism, constants.PassphraseArgonDigestSize)
 	return digest
 }
